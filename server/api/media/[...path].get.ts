1c3ee5b6586dee3c841083e561faa358c54ed8bc
Dreamer DeLy <dreamerandprogrammer@gmail.com>
2025-10-13 07:00:17 +0300

    Fix loading files with cyrillic characters in path

--- server/api/media/[...path].get.ts
+++ server/api/media/[...path].get.ts
@@ -7,6 +7,7 @@ export default defineEventHandler((event) => {
 	const pathParam = event.context.params?.path;
 	if (!pathParam) throw createError({ statusCode: 400, message: "Missing path" });
 
-	const filePath = join(PROJECTS_DIR, ...(Array.isArray(pathParam) ? pathParam : [pathParam]));
+	const decodedPath = Array.isArray(pathParam) ? pathParam.map(decodeURIComponent) : [decodeURIComponent(pathParam)];
+	const filePath = join(PROJECTS_DIR, ...decodedPath);
 	return sendStream(event, createReadStream(filePath));
 });
